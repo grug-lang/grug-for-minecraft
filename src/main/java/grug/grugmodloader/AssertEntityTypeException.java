8f4f4e770e21889337162357e5d309932ceef522
Sander Bos <sander@streamhpc.com>
2026-08-16 14:05:36 +0200

    cleanup: start fresh

--- src/main/java/grug/grugmodloader/AssertEntityTypeException.java
+++
@@ -1,7 +0,0 @@
-package grug.grugmodloader;
-
-public class AssertEntityTypeException extends Exception {
-    public AssertEntityTypeException(GrugEntityType entityType, GrugEntityType expectedEntityType) {
-        super("Expected " + StringUtils.getSnakeCase(expectedEntityType) + ", but got " + StringUtils.getSnakeCase(entityType));
-    }
-}
